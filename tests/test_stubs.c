946c7c4cd9f27928596deba12445a4a4d57e2248
joexbayer <jb71999@gmail.com>
2026-01-31 10:50:50 +0100

    Adds missing stubs for tests

--- tests/test_stubs.c
+++ tests/test_stubs.c
@@ -1,4 +1,5 @@
 #include <router.h>
+#include <engine.h>
 
 struct job_route router_job_find(struct router *router, const char *module_name, const char *job_name) {
     (void)router;
@@ -11,4 +12,21 @@ void router_job_release(struct router *router, struct module_ref *ref, struct cw
     (void)router;
     (void)ref;
     (void)ctx;
+}
+
+int safe_execute_job_run(job_run_t handler, struct cweb_context *ctx, const job_payload_t *payload, job_ctx_t *job, int *rc_out) {
+    if (!handler) {
+        return -1;
+    }
+    int rc = handler(ctx, payload, job);
+    if (rc_out) {
+        *rc_out = rc;
+    }
+    return 0;
+}
+
+void safe_execute_job_cancel(void (*cancel)(struct cweb_context *ctx, const char *job_uuid), struct cweb_context *ctx, const char *job_uuid) {
+    if (cancel) {
+        cancel(ctx, job_uuid);
+    }
 }
